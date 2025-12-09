369a3d202b148e9bdcead93376d3a06e73ea4c93
Doyeon77 <vegakdy@gmail.com>
2025-12-10 02:47:55 +0900

    깃에서 medication-manager 폴더 전체 삭제

--- Desktop/리엑트/medication-manager/src/pages/Settings.jsx
+++
@@ -1,9 +0,0 @@
-export default function Settings() {
-  return (
-    <div className="p-8 max-w-2xl mx-auto">
-      <h2 className="text-3xl font-extrabold mb-8 text-center text-blue-800">
-        설정 기능이 비활성화되었습니다.
-      </h2>
-    </div>
-  );
-}
